0bf62fbc90ec6d8f283f7ef5be7b1f9566264214
Faraz Basil <farazbasil@gmail.com>
2020-07-12 11:56:52 +0500

    Launch My React App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Test from './test.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div className = "App" >
+        <
+        Test dishName = "Biryani"
+        sweetDish = "Labe Shereen" / >
+        <
+        hr / >
+        <
+        Test dishName = "Qorma"
+        sweetDish = "Kheer" / >
+        <
+        hr / >
+        <
+        Test dishName = "B.B.Q"
+        sweetDish = "Loki Ka Halwa" / >
+
+
+        <
+        /div >
+
+    );
 }
 
 export default App;
---
+++ src/test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function test(props) {
+    return ( < div > < h1 > Do You Want { props.dishName } ? < /h1>  <
+        h1 > Do you Want { props.sweetDish } ? < /h1> < /
+        div > )
+
+
+}
+export default test;
